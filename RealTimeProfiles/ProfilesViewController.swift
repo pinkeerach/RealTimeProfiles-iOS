55d8541277ce2897b5bf6a22e1282bf04b62903d
pinkeerach <madEthelFlint@gmail.com>
2018-08-15 19:58:09 -0400

    initial commit

---
+++ RealTimeProfiles/ProfilesViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RealTimeProfiles
+//
+//  Created by Rachel Parsons on 8/15/18.
+//  Copyright © 2018 Rachel Parsons. All rights reserved.
+//
+
+import UIKit
+
+class ProfilesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ RealTimeProfilesTests/RealTimeProfilesTests.swift
@@ -0,0 +1,36 @@
+//
+//  RealTimeProfilesTests.swift
+//  RealTimeProfilesTests
+//
+//  Created by Rachel Parsons on 8/15/18.
+//  Copyright © 2018 Rachel Parsons. All rights reserved.
+//
+
+import XCTest
+@testable import RealTimeProfiles
+
+class RealTimeProfilesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
